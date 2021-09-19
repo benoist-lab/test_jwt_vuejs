842f36bd6151e6244de5d575e94220123fb553ef
Dubuc Benoist <dubucbenoist@yahoo.fr>
2021-09-19 18:48:04 +0200

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,124 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+			<form>
+				<div class="form-group">
+					<label for="login">ID</label>
+					<input id="login" v-model="formConnection.username" class="form-control">
+				</div>
+
+				<div class="form-group">
+					<label for="password">ID</label>
+					<input id="password" v-model="formConnection.password" class="form-control">
+				</div>
+				
+				<div class="d-flex flex-row-reverse m-3">
+					<div v-on:click.stop="connecter" class="btn btn-primary">Connecter</div>
+				</div>
+
+			</form>
+			
+			<p>message connexion: {{ messageConnexion }}</p>
+			<p>token: {{ token }}</p>
+    
+
+			<div v-on:click.stop="getMessageServeur" class="btn btn-primary">Message Serveur</div>
+			<p>message serveur: {{ messageServeur }}</p>
+
+    
+    
+    
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data () {
+		return {
+			formConnection: {
+				username: "javainuse",
+				password: "password"
+			},
+			messageConnexion: "aucun",
+			token: "",
+			messageServeur: ""
+		}
+	},
+	methods: {
+		connecter: function () {
+			console.log ("DEBUT CONNECTION");
+			
+
+			
+			
+			axios.post ("http://localhost:8080/authenticate",this.formConnection)
+			.then (response=>{
+				console.log ("connection acceptee")
+				console.log (response);
+				
+				this.messageConnexion="connection OK";
+				this.token=response.data.jwttoken
+				
+			})
+			.catch (exception =>{
+				console.log(exception)
+				
+				this.messageConnexion="connection ERREUR";
+
+			})
+			
+		},
+		getMessageServeur: function () {
+			console.log ("DEBUT MESSAGE SERVEUR");
+				
+/* 			const config = {
+					headers: {
+						//Accept: 'application/json',
+						//'Content-Type': 'application/json',
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+    
+		"Access-Control-Allow-Origin": "*",
+		"access-control-allow-credentials": "true",
+		
+						Authorization: "Bearer "+this.token
+					}
+			}; */
+
+			
+			const config = {
+					headers: {
+						Authorization: "Bearer "+this.token
+					}
+			};
+			
+			console.log ("config="+config.headers.Authorization);
+
+			
+			axios.get ("http://localhost:8080/hello",config)
+			.then (response=>{
+				console.log ("message recu")
+				console.log (response);
+				
+				this.messageServeur=response.data
+				
+			})
+			.catch (exception =>{
+				console.log(exception)
+				
+				this.messageConnexion="message serveur ERREUR";
+
+			})
+			
+		}
+	}
 }
 </script>
 
@@ -44,15 +127,5 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+/*
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:8080', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+Vue.use(cors(corsOptions));
+*/
+
 Vue.config.productionTip = false
 
 new Vue({
